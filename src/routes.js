3e01457e5246c07e702871e8295299449b979de0
rebecalazarini <00001129785208sp@al.educacao.sp.gov.br>
2024-11-13 16:18:41 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const routes = express.Router();
+
+const funcionarios = require('./controller/funcionarios')
+const cargos = require('./controller/cargos')
+const departamentos = require('./controller/departamentos')
+
+routes.post('/funcionarios',funcionarios.create)
+routes.get('/funcionarios',funcionarios.read)
+routes.put('/funcionarios/:id',funcionarios.update)
+routes.delete('/funcionarios/:id',funcionarios.deletar)
+
+routes.post('/cargos',cargos.create)
+routes.get('/cargos',cargos.read)
+routes.put('/cargos/:id',cargos.update)
+routes.delete('/cargos/:id',cargos.deletar)
+
+routes.post('/departamentos',departamentos.create)
+routes.get('/departamentos',departamentos.read)
+routes.put('/departamentos/:id',departamentos.update)
+routes.delete('/departamentos/:id',departamentos.deletar)
+
+module.exports = routes
